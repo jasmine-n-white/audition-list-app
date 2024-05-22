b5a1599c774840ffb2a486686ef393e454399074
Jasmine White <jasmine@whitepdx.com>
2024-05-22 12:13:35 -0500

    formatting header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <>
+    <header className="headerFormat">
+        <h1>Title</h1>
+    </header>
+    </>
+  )
+}
+
+export default Header
